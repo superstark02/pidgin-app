14494a2815afd470c84f360c6aea6671f6fc22df
superstark02 <superstark02@gmail.com>
2020-08-22 11:56:29 +0530

    8/22/2020

---
+++ client/build/precache-manifest.3d8764337a0377b52fde4316d1b44cf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e32eaa7d880440806fa44c73155c6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e519a4a7ec6ba9ed0a14",
+    "url": "/static/css/main.fee5117b.chunk.css"
+  },
+  {
+    "revision": "2e8c99dc8ecb928584d2",
+    "url": "/static/js/2.e0bf35b1.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/static/js/2.e0bf35b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e519a4a7ec6ba9ed0a14",
+    "url": "/static/js/main.d874a1ef.chunk.js"
+  },
+  {
+    "revision": "4f7447aa82634759df62",
+    "url": "/static/js/runtime-main.b585c263.js"
+  },
+  {
+    "revision": "b5f0af33f35b37afe1f76cc269f31ec6",
+    "url": "/static/media/Default.b5f0af33.otf"
+  },
+  {
+    "revision": "77d92bf236381a2ac02447b177e3130f",
+    "url": "/static/media/Thin.77d92bf2.otf"
+  }
+]);
